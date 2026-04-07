7dab0e950ae874d5c1e108bb6c3301f15fb1ccbf
kauramadhav50 <159455626+kauramadhav50@users.noreply.github.com>
2026-04-07 13:40:44 +0530

    hello

--- udesya/settings.py
+++ udesya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q9iz4$aio_rht7#)8ed)&(_=$e*h=32-bk3j_p9e6cu@e(o66_
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
